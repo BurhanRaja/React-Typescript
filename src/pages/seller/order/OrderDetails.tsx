1cd10c311601182d503c90bb591e564d16062b82
BurhanRaja <burhanraja02@yahoo.com>
2023-05-29 09:09:38 +0530

    debugged

--- src/pages/seller/order/OrderDetails.tsx
+++ src/pages/seller/order/OrderDetails.tsx
@@ -20,8 +20,6 @@ const OrderDetails = () => {
     dispatch(getSingleSellerOrderThunk(id));
   }, []);
 
-  console.log(sellerorder.product);
-
   return (
     <>
       <div className="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto">
